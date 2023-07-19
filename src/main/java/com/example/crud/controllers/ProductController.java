1b692f2585a4ea338a92a83121e8ea52acaf564e
Natan <zerogamerptbr@gmail.com>
2023-07-19 13:24:10 -0300

    Creating a table and controller

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -1,8 +1,15 @@
 package com.example.crud.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class ProductController {
-    
+    @GetMapping
+    public ResponseEntity getAllProducts(){
+        return ResponseEntity.ok("deu tudo certo");
+    }
 }
